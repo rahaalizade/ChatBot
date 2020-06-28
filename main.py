6d2f14050c453f7b4299fba7483265ec04474a73
FAT <Alizadeh.fatemeh@yahoo.com>
2020-06-28 12:46:52 +0430

    label files and loop through our Json File

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ import tflearn
 import tensorflow
 import random
 import json
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
+        
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
