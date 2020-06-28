0f1571695eb5391db59ee5cbc0a7fd93d0b8eb87
FAT <Alizadeh.fatemeh@yahoo.com>
2020-06-28 21:34:26 +0430

    creating our model and training it

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
         
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
@@ -54,3 +54,12 @@ for x, doc in enumerate(docs_x):
     
 training = numpy.array(training)
 output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]),activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
